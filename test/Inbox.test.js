07798f3ddc90535e5a52236cad3158bf1fe9dc23
Harsh Kedia <harshkedia05@gmail.com>
2018-08-16 15:58:11 +0200

    minor changes

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,53 +1,27 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const { interface, bytecode } = require('../compile');
 
-const web3 = new Web3(ganache.provider());  // A web3 instance
+const provider = ganache.provider();
+const web3 = new Web3(provider);  // A web3 instance
 
-
-beforeEach(() => {
+let accounts;
+let inbox;
+beforeEach(async () => {
     // Get a list of accounts
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-        });
+    accounts = await web3.eth.getAccounts();
 
     // Use one to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))  //tells web3 what methods Inbox contract has
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })   //tells web3 to deploy a new copy of the contract
+        .send({ from: accounts[0], gas: '1000000' });           //tells web3 to send a transaction and create the contract
+
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-
+        assert.ok(inbox.options.address);
     });
 });
-
-
-
-
-
-
-// Sample Test Code
-// class Car {
-//     park() {
-//         return 'stopped';
-//     }
-
-//     drive() {
-//         return 'vroom';
-//     }
-// }
-
-// let car;
-// beforeEach(() => {
-//     car = new Car();
-// });
-
-// describe('Car', () => {
-//     it('can park', () => {
-//         assert.equal(car.park(), 'stopped');
-//     });
-
-//     it('can drive', () => {
-//         assert.equal(car.drive(), 'vroom');
-//     });
-// });
