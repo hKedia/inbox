2b7424b10a5d6018a03661549f8637617d136b6f
Harsh Kedia <harshkedia05@gmail.com>
2018-08-16 15:21:17 +0200

    sample test for Inbox contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,11 +2,24 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
-const web3 = new Web3(ganache.provider());
+const web3 = new Web3(ganache.provider());  // A web3 instance
 
 
+beforeEach(() => {
+    // Get a list of accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
 
+    // Use one to deploy the contract
+});
 
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+
+    });
+});
 
 
 
