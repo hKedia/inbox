31e74093dc7b170be25cc9252eae11e2a700023a
Harsh Kedia <harshkedia05@gmail.com>
2018-08-17 15:21:46 +0200

    Deploying to rinkeby using infura api

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -16,23 +16,24 @@ beforeEach(async () => {
     // Use one to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))  //tells web3 what methods Inbox contract has
         .deploy({ data: bytecode, arguments: [INITIAL_STRING] })   //tells web3 to deploy a new copy of the contract
-        .send({ from: accounts[0], gas: '1000000' });           //tells web3 to send a transaction and create the contract
+        .send({ from: accounts[0], gas: '10000000' });           //tells web3 to send a transaction and create the contract
 
     inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
+    // testing if the contract was created
     it('deploys a contract', () => {
-        assert.ok(inbox.options.address);
+        assert.ok(inbox.options.address);   
     });
-
+    // testing whether the contract has the initial value
     it('has a default message', async () => {
         const message = await inbox.methods.message().call();
         assert.equal(message, INITIAL_STRING);
     });
-
+    // testing whether the setMessage is working
     it('can change the message', async () => {
-        await inbox.methods.setMessage('Bye there!').send({ from: accounts[0], gas: '100000' });
+        await inbox.methods.setMessage('Bye there!').send({ from: accounts[0], gas: '1000000' });
         const message = await inbox.methods.message().call();
         assert.equal(message, 'Bye there!');
     });
